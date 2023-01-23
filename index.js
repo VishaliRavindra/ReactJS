6c7b00d3a723e7115332a3585a4fc18448637790
vishala <hishitha.kuracha@gmail.com>
2023-01-23 09:32:15 +0000

    practise 1 Responsive Web Design

---
+++ index.js
@@ -0,0 +1,24 @@
+const element = (
+  //   Write your code here.
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="bgColor">
+          <h1 class="heading">Super Over League</h1>
+          <div class="d-flex justify-content-center flex-row">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+              class="images"
+            />
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+              class="images"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
